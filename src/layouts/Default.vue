2cf25e59cdf75cb73dd5bc60f72d1f965cd0c6df
Morgan VanDerLeest <morgan@atomicdigital.io>
2019-11-13 21:01:50 -0600

    cleaning up

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -37,7 +37,7 @@
             <g-link to="/docs/" class="text-copy-primary hover:text-gray-600">Docs</g-link>
           </li> -->
           <li>
-            <g-link to="/blog/" class="text-copy-primary hover:text-gray-600">Blog</g-link>
+            <g-link to="/blog" class="text-copy-primary hover:text-gray-600">Blog</g-link>
           </li>
         </ul>
       </nav>
