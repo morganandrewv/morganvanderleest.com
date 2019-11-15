751dca6745fb7def8d558ac81ff70b57af0e6bc6
Morgan VanDerLeest <morgan@atomicdigital.io>
2019-11-15 08:23:57 -0600

    removing link powers so the link... works

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -37,7 +37,8 @@
             <g-link to="/docs/" class="text-copy-primary hover:text-gray-600">Docs</g-link>
           </li> -->
           <li>
-            <g-link to="/blog" class="text-copy-primary hover:text-gray-600">Blog</g-link>
+            <!-- <g-link to="/blog" class="text-copy-primary hover:text-gray-600">Blog</g-link> -->
+            <a to="/blog" class="text-copy-primary hover:text-gray-600">Blog</a>
           </li>
         </ul>
       </nav>
