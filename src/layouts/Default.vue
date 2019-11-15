8b914438e8f21b1aa4df01264ae9e979062348dd
Morgan VanDerLeest <morgan@atomicdigital.io>
2019-11-15 08:14:32 -0600

    Revert "header logo test to fix blog link"
    This reverts commit 50ed389c234d990e1f407fce3ec5ecb618512fbe.

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -3,7 +3,8 @@
     <header class="border-t-14 border-green-700">
       <nav class="container mx-auto flex flex-wrap justify-between items-center py-8">
         <div>
-          <header-logo />
+          <g-link v-if="theme === 'theme-light'" to="/"><g-image src="../../static/logo.svg" class="w-40" alt="logo" /></g-link>
+          <g-link v-else to="/"><g-image src="../../static/logo_dark_mode.svg" class="w-40" alt="logo" /></g-link>
         </div>
         <div class="block lg:hidden">
           <button @click="toggle" class="flex items-center px-3 py-2 border rounded border-gray-500 hover:text-gray-600 hover:border-gray-600">
@@ -105,13 +106,11 @@ query {
 <script>
 import SearchInput from '../components/SearchInput'
 import ThemeSwitcher from '../components/ThemeSwitcher'
-import HeaderLogo from '../components/HeaderLogo'
 
 export default {
   components: {
     SearchInput,
-    ThemeSwitcher,
-    HeaderLogo
+    ThemeSwitcher
   },
   mounted() {
     this.theme = localStorage.getItem('theme') || 'theme-light'
